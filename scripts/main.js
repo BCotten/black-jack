d5d85025c81d6711a43837b2731e97728c60d583
Brandon Cotten <brandon__cotten@hotmail.com>
2024-11-08 18:57:07 -0800

    added some notes to js file about whats needed

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
 console.log("Script Loading...");
+
+// class for the cards shuffle and place
+//function to take card svg and make a array with black jack card value and the svg file path
+
+//function to shuffle the cards and put in to shuffled array
+
+//
+
+// class for the player information name, card backing{s}
+
+// game class for the main game play track score, check score
+
+// needs function to check score
+// player gets two cards then current score should be shown with option to hold or get a card
+// over 21 automatic loss
+// Ace worth 1 or 11 Auto worth 1 if hand over 21
+// king queen jack worth 10
+// 2 to 10 worth their card number
