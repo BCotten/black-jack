3441ecbafe5267c2dc1d9366f80e50f1957ef55e
Brandon Cotten <brandon__cotten@hotmail.com>
2024-11-13 12:03:42 -0800

    changed card array to object working on some fixes for shuffle and deck and then move back to score-function

--- scripts/main.js
+++ scripts/main.js
@@ -55,27 +55,74 @@ const deck = [
   [11, "AH"],
 ];
 
-console.log(deck);
-/* deck[spot 0-51][0 -value] deck[spot 0-51][1 -cardType] */
-console.log(deck[52])
+const deckObject = [
+  { suit: "2H", value: 2 },
+  { suit: "3H", value: 3 },
+  { suit: "4H", value: 4 },
+  { suit: "5H", value: 5 },
+  { suit: "6H", value: 6 },
+  { suit: "7H", value: 7 },
+  { suit: "8H", value: 8 },
+  { suit: "9H", value: 9 },
+  { suit: "TH", value: 10 },
+  { suit: "JH", value: 10 },
+  { suit: "QH", value: 10 },
+  { suit: "KH", value: 10 },
+  { suit: "AH", value: 11 },
 
-function shuffleDeck(deck) {
-  const shuffledDeck = [];
-  let length = deck.length - 1;
+  { suit: "2D", value: 2 },
+  { suit: "3D", value: 3 },
+  { suit: "4D", value: 4 },
+  { suit: "5D", value: 5 },
+  { suit: "6D", value: 6 },
+  { suit: "7D", value: 7 },
+  { suit: "8D", value: 8 },
+  { suit: "9D", value: 9 },
+  { suit: "TD", value: 10 },
+  { suit: "JD", value: 10 },
+  { suit: "QD", value: 10 },
+  { suit: "KD", value: 10 },
+  { suit: "AD", value: 11 },
 
-  while (0 < length) {
-    const randomCard = Math.floor(Math.random() * length + 1);
-    console.log(randomCard);
-    let current = deck.splice(randomCard, 1);
-    console.log(current);
-    shuffledDeck.push(current);
-    length--;
-  }
-  return shuffledDeck;
-}
+  { suit: "2C", value: 2 },
+  { suit: "3C", value: 3 },
+  { suit: "4C", value: 4 },
+  { suit: "5C", value: 5 },
+  { suit: "6C", value: 6 },
+  { suit: "7C", value: 7 },
+  { suit: "8C", value: 8 },
+  { suit: "9C", value: 9 },
+  { suit: "TC", value: 10 },
+  { suit: "JC", value: 10 },
+  { suit: "QC", value: 10 },
+  { suit: "KC", value: 10 },
+  { suit: "AC", value: 11 },
 
-const shuffledDeck = shuffleDeck(deck);
+  { suit: "2S", value: 2 },
+  { suit: "3S", value: 3 },
+  { suit: "4S", value: 4 },
+  { suit: "5S", value: 5 },
+  { suit: "6S", value: 6 },
+  { suit: "7S", value: 7 },
+  { suit: "8S", value: 8 },
+  { suit: "9S", value: 9 },
+  { suit: "TS", value: 10 },
+  { suit: "JS", value: 10 },
+  { suit: "QS", value: 10 },
+  { suit: "KS", value: 10 },
+  { suit: "AS", value: 11 },
+];
 
+const startBtn = document.querySelector(".startBtn");
+const hitBtn = document.querySelector(".hitBtn");
+const holdBtn = document.querySelector(".holdBtn");
+const aceBtn = document.querySelector(".aceBtn");
+
+const playerScoreBoard = document.querySelector(".playerScore");
+const dealerScoreBoard = document.querySelector(".dealerScore");
+
+let playerScore = 0;
+let dealerScore = 0;
 
 // class for the cards shuffle and place
 //function to take card svg and make a array with black jack card value and the svg file path
@@ -83,6 +130,54 @@ const shuffledDeck = shuffleDeck(deck);
 //function to shuffle the cards and put in to shuffled array
 
 //
+for (let i = 0; i < deckObject.length; i++) {
+  console.log(deckObject[i]);
+}
+
+console.log(deckObject[22].suit, deckObject[22].value)
+
+function shuffleDeck(deck) {
+  const deckShuffled = {};
+  const length = deck.length;
+
+}
+
+/* function shuffleDeck(deck) {
+  const shuffledDeck = []; have insert arrays into this empty array;
+  let length = deck.length - 1;
+  let count = 0;
+
+  while (0 <= length) {
+    const randomCard = Math.floor(Math.random() * length + 1);
+    let current = deck.splice(randomCard, 1);
+    change this to input into array shuffledDeck.push(current);
+    shuffledDeck.splice([count], 0, current);
+    length--;
+    count++;
+  }
+  return shuffledDeck;
+} */
+
+// one face up to player -- one face down to dealer
+// one face up to player -- one face up to dealer
+function firstDeal(shuffledDeck) {
+  console.log(shuffledDeck);
+  for (let i = 0; i < 4; i++) {
+    const arrayEnd = shuffledDeck.length - 1;
+    let current = shuffleDeck[arrayEnd];
+    shuffleDeck.pop;
+    if (i % 2) {
+      playerScore = playerScore + current[0];
+      // place card in html
+    } else {
+      if (i === 1) {
+        //place dealer card with background on top
+      }
+      dealerScore = dealerScore + current[0];
+      // place dealer card faceup
+    }
+  }
+}
 
 // class for the player information name, card backing{s}
 
