0ef7c7434069fe139c9ca6a41641a7b8ca718e34
Brandon Cotten <brandon__cotten@hotmail.com>
2024-11-17 19:41:07 -0800

    fixed previous issue of random function and initial part of first deal is done animation and visuals to be done later next is playerHit function

--- scripts/main.js
+++ scripts/main.js
@@ -1,61 +1,6 @@
 console.log("Script Loading...");
 
 const deck = [
-  [2, "2S"],
-  [3, "3S"],
-  [4, "4S"],
-  [5, "5S"],
-  [6, "6S"],
-  [7, "7S"],
-  [8, "8S"],
-  [9, "9S"],
-  [10, "TS"],
-  [10, "JS"],
-  [10, "QS"],
-  [10, "KS"],
-  [11, "AS"],
-  [2, "2C"],
-  [3, "3C"],
-  [4, "4C"],
-  [5, "5C"],
-  [6, "6C"],
-  [7, "7C"],
-  [8, "8C"],
-  [9, "9C"],
-  [10, "TC"],
-  [10, "JC"],
-  [10, "QC"],
-  [10, "KC"],
-  [11, "AC"],
-  [2, "2D"],
-  [3, "3D"],
-  [4, "4D"],
-  [5, "5D"],
-  [6, "6D"],
-  [7, "7D"],
-  [8, "8D"],
-  [9, "9D"],
-  [10, "TD"],
-  [10, "JD"],
-  [10, "QD"],
-  [10, "KD"],
-  [11, "AD"],
-  [2, "2H"],
-  [3, "3H"],
-  [4, "4H"],
-  [5, "5H"],
-  [6, "6H"],
-  [7, "7H"],
-  [8, "8H"],
-  [9, "9H"],
-  [10, "TH"],
-  [10, "JH"],
-  [10, "QH"],
-  [10, "KH"],
-  [11, "AH"],
-];
-
-const deckObject = [
   { suit: "2H", value: 2 },
   { suit: "3H", value: 3 },
   { suit: "4H", value: 4 },
@@ -121,8 +66,10 @@ const aceBtn = document.querySelector(".aceBtn");
 const playerScoreBoard = document.querySelector(".playerScore");
 const dealerScoreBoard = document.querySelector(".dealerScore");
 
-let playerScore = 0;
-let dealerScore = 0;
+const score = {
+  playerScore: 0,
+  dealerScore: 0,
+};
 
 // class for the cards shuffle and place
 //function to take card svg and make a array with black jack card value and the svg file path
@@ -130,53 +77,58 @@ let dealerScore = 0;
 //function to shuffle the cards and put in to shuffled array
 
 //
-for (let i = 0; i < deckObject.length; i++) {
-  console.log(deckObject[i]);
+for (let i = 0; i < deck.length; i++) {
+  console.log(deck[i]);
 }
 
-console.log(deckObject[22].suit, deckObject[22].value)
+console.log(deck[22].suit, deck[22].value);
+console.log(deck.length);
 
 function shuffleDeck(deck) {
-  const deckShuffled = {};
-  const length = deck.length;
+  const shuffledDeck = [];
+  let length = deck.length;
 
-}
-
-/* function shuffleDeck(deck) {
-  const shuffledDeck = []; have insert arrays into this empty array;
-  let length = deck.length - 1;
-  let count = 0;
-
-  while (0 <= length) {
-    const randomCard = Math.floor(Math.random() * length + 1);
-    let current = deck.splice(randomCard, 1);
-    change this to input into array shuffledDeck.push(current);
-    shuffledDeck.splice([count], 0, current);
+  while (length > 0) {
+    const randomCard = Math.floor(Math.random() * length);
+    let current = deck.splice(randomCard, 1)[0];
+    shuffledDeck.push(current);
     length--;
-    count++;
   }
   return shuffledDeck;
-} */
+}
+
+const shuffledDeck = shuffleDeck(deck);
+
+console.log(shuffledDeck);
 
 // one face up to player -- one face down to dealer
 // one face up to player -- one face up to dealer
 function firstDeal(shuffledDeck) {
-  console.log(shuffledDeck);
-  for (let i = 0; i < 4; i++) {
-    const arrayEnd = shuffledDeck.length - 1;
-    let current = shuffleDeck[arrayEnd];
-    shuffleDeck.pop;
+  const arrayEnd = shuffledDeck.length - 1;
+  for (let i = arrayEnd; i > arrayEnd - 4; i--) {
+    let current = shuffledDeck[i];
+    shuffledDeck.pop;
     if (i % 2) {
-      playerScore = playerScore + current[0];
+      score.playerScore = score.playerScore + current.value;
+      playerScoreBoard.innerHTML = score.playerScore;
       // place card in html
     } else {
       if (i === 1) {
         //place dealer card with background on top
       }
-      dealerScore = dealerScore + current[0];
+      score.dealerScore = score.dealerScore + current.value;
+      dealerScoreBoard.innerHTML = score.dealerScore;
       // place dealer card faceup
     }
   }
+}
+
+firstDeal(shuffledDeck);
+console.log(score.playerScore);
+console.log(score.dealerScore);
+
+function playerHit(shuffledDeck) {
+  // allow play to hit until at 21 (they won) or over 21 (they lost) or they hold
 }
 
 // class for the player information name, card backing{s}
